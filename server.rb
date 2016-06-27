e974ccb82642d1983c25546cdac06a6603906a87
MaraJade <catbookgirl@gmail.com>
2016-06-27 10:19:37 -0700

    Separating out the commits

--- server.rb
+++ server.rb
@@ -4,4 +4,7 @@ require 'json'
 post '/payload' do
   push = JSON.parse(request.body.read)
   puts "I got some JSON: #{push.inspect}"
+
+  push[:commits].each do |commit|
+      puts "#{commit}"
 end
