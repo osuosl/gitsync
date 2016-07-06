59afe600ea6542c29328eedecd481805b9956ad2
MaraJade <catbookgirl@gmail.com>
2016-07-06 16:42:38 -0700

    Realized that curl is a good way to test, need push data

--- server.rb
+++ server.rb
@@ -3,29 +3,35 @@ require 'sinatra'
 require 'json'
 require 'rubygems'
 require 'pp'
-require_relative 'rimesync'
+require_relative 'rimesync/rimesync.rb'
 
 post '/payload' do
-    #ts = rimesync.Timesync("http://timesync-staging.osuosl.org/v0/")
+    #ts = Timesync.new(baseurl="http://timesync-staging.osuosl.org/v0/")
     #ts.authenticate(username="test", password="test", auth_type="password")
 
-    push = JSON.parse(request.body.read)
+    puts "#{request.body.read}"
+    #push = JSON.parse(request.body.read)
+    #puts "#{push}"
 
-    push["commits"].each do |commit|
-        #puts "Author: #{commit["author"]}"
-        #puts "Committer: #{commit["committer"]}"
-        message = commit["message"].split(/\n/)
-        #puts "#{message}"
-        user = message[2].split(' ', 2)
-        project = message[3].split(' ', 2)
-        duration = message[4].split(' ', 2)
-        activities = message[5].split(' ', 2)
-        issue = message[6].split(' ', 2)
-        
-        puts "User: #{user}"
-        puts "Project: #{project}"
-        puts "Duration: #{duration}"
-        puts "Activities: #{activities}"
-        puts "Issue: #{issue}"
-    end
+    #push["commits"].each do |commit|
+    #    #puts "Author: #{commit["author"]}"
+    #    #puts "Committer: #{commit["committer"]}"
+    #    message = commit["message"].split(/\n/)
+    #    #puts "#{message}"
+    #    user = commit["author"]["username"]
+    #    project = message[3].split(' ', 2)
+    #    duration = message[4].split(' ', 2)
+    #    activities = message[5].split(' ', 2)
+    #    issue = message[6].split(' ', 2)
+    #    date_worked = commit["timestamp"]
+    #    notes = message[0]
+    #    
+    #    puts "User: #{user}"
+    #    puts "Project: #{project}"
+    #    puts "Duration: #{duration}"
+    #    puts "Activities: #{activities}"
+    #    puts "Issue: #{issue}"
+    #    puts "Date Worked: #{date_worked}"
+    #    puts "Notes: #{notes}"
+    #end
 end
