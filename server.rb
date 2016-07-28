a7ab2aa6b9cc61ab2616f437470bac539cdcc68a
MaraJade <catbookgirl@gmail.com>
2016-07-28 13:12:31 -0700

    Should handle multiple commits now

--- server.rb
+++ server.rb
@@ -11,44 +11,35 @@ post '/payload' do
 
     push = JSON.parse(request.body.read)
 
-    # Ruby takes local scoping a little *too* literally
-    user, duration, activities, issue_uri, date_worked, notes, slug = nil
 
     push["commits"].each do |commit|
         # Get all the necessary time data
         message = commit["message"].split(/\n/)
-        user = commit["author"]["username"]
-        duration = message[2].split(' ', 2)[1].to_i
-        activities = message[3].split(' ', 2)[1].split(',')
-        issue_uri = message[4].split(' ', 2)[1]
-        date_worked = commit["timestamp"].split('T')[0]
-        notes = message[0]
-        
-    end
 
-    project_name = push["repository"]["name"]
-    projects = ts.get_projects()
+        project_name = push["repository"]["name"]
+        projects = ts.get_projects()
 
-    # Get the slug of the project
-    projects.each do |project|
-        if project['name'].downcase == project_name.downcase
-            slug = project['slugs'][0]
+        # Ruby takes local scoping a little *too* literally
+        slug = nil
+        # Get the slug of the project
+        projects.each do |project|
+            if project['name'].downcase == project_name.downcase
+                slug = project['slugs'][0]
+            end
         end
+
+        time = {
+            'user' => commit["author"]["username"],
+            'duration' => message[2].split(' ', 2)[1].to_i,
+            'project' => slug,
+            'activities' => message[3].split(' ', 2)[1].split(','),
+            'issue_uri' => message[4].split(' ', 2)[1],
+            'date_worked' => commit["timestamp"].split('T')[0],
+            'notes' => message[0]
+        }
+
+        res = ts.create_time(time)
     end
-
-    # Create the time
-    time = {
-        'duration' => duration,
-        'user' => user,
-        'project' => slug,
-        'activities' => activities,
-        'date_worked' => date_worked,
-        'issue_uri' => issue_uri,
-        'notes' => notes
-    }
-
-    # Submit the time
-    res = ts.create_time(time)
 
     # This prevents a NoMethodError for some reason
     # Something about the return value being a string?
