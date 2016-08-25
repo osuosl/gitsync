18cb1633b8fdf9f84137964c42c639b0e1c42562
MaraJade <catbookgirl@gmail.com>
2016-08-25 10:29:18 -0700

    Commented the code some more

--- server.rb
+++ server.rb
@@ -6,28 +6,35 @@ require 'pp'
 require_relative 'rimesync/lib/rimesync.rb'
 
 post '/payload' do
+    # Auth with Timesync. This will need to be removed once a real auth system
+    # is implemented
     ts = TimeSync.new(baseurl="http://timesync-staging.osuosl.org/v0/")
     ts.authenticate(username:"test", password:"test", auth_type:"password")
 
+    # Parses the data for reading
     push = JSON.parse(request.body.read)
 
-
+    # Goes through each commit from the push and submits a time for it
     push["commits"].each do |commit|
-        # Get all the necessary time data
+        # Get all the necessary data from the commit message
         message = commit["message"].split(/\n/)
 
+        # Gets the name of the repository, which should be the name of the
+        # project
         project_name = push["repository"]["name"]
         projects = ts.get_projects()
 
         # Ruby takes local scoping a little *too* literally
         slug = nil
         # Get the slug of the project
+        # Have to do this the hard way because there's no easy way to do it
         projects.each do |project|
             if project['name'].downcase == project_name.downcase
                 slug = project['slugs'][0]
             end
         end
 
+        # Creates the time object
         time = {
             'user' => commit["author"]["username"],
             'duration' => message[2].split(' ', 2)[1].to_i,
@@ -38,10 +45,12 @@ post '/payload' do
             'notes' => message[0]
         }
 
+        # Submit the time
         res = ts.create_time(time)
     end
 
     # This prevents a NoMethodError for some reason
     # Something about the return value being a string?
+    # This gets printed to the console so you know everything's been completed
     "done\n"
 end
