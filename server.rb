f72db80e72a04b8bf93027632ad797779af88f35
Aaron Cohen <cohenaa@osuosl.org>
2016-09-01 11:55:05 -0700

    Use config file with temporary authentication solution

--- server.rb
+++ server.rb
@@ -6,27 +6,28 @@ require 'rubygems'
 require 'pp'
 require_relative 'rimesync/lib/rimesync.rb'
 
-config_defaults = {
-  :baseurl => nil,
-  :username => nil,
-  :password => nil,
-  :ldap => false,
-}
-
 def parse_config(config_path = "config.yml")
-  config = config_defaults.clone
+  config = {
+    :baseurl => nil,
+    :username => nil,
+    :password => nil,
+    :auth_type => "password",
+  }
 
+  # Read "config.yml" and update the config hash with its values
   config.merge! Hash[YAML::load(open(config_path)).map {|k, v| [k.to_sym, v]}]
 
-  print config
+  config
 end
 
 post '/payload' do
-  parse_config
+  # Parse "config.yml" for server/authentication configuration values
+  config = parse_config
+
   # Auth with Timesync. This will need to be removed once a real auth system
   # is implemented
-  ts = TimeSync.new(baseurl="http://timesync-staging.osuosl.org/v0/")
-  ts.authenticate(username:"test", password:"test", auth_type:"password")
+  ts = TimeSync.new(baseurl=config[:baseurl])
+  ts.authenticate(username:config[:username], password:config[:password], auth_type:config[:auth_type])
 
   # Parses the data for reading
   push = JSON.parse(request.body.read)
